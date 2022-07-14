b5cb8d2f217be327b4a776b569dc68d481b51faa
Šimon Macek <simon.macek@proficio.cz>
2022-07-14 15:29:45 +0300

    create a dynamic list of projects

---
+++ components/content/ProjectList.vue
@@ -0,0 +1,16 @@
+<template>
+<pre>Component Project List</pre>
+  <ul v-if="data.length" class="list-style-none">
+    <li v-for="item in data" :key="item._id">
+      <h2>{{item.title}}</h2>
+      <div>
+        {{item.description}}
+      </div>
+      <img :src="item.fields?.image" :alt="item.fields?.imageAlt">
+    </li>
+  </ul>
+</template>
+
+<script setup>
+const { data } = await useAsyncData('portfolio', () => queryContent('/portfolio').find())
+</script>
--- content/2.portfolio/index.md
+++ content/2.portfolio/index.md
@@ -4,7 +4,5 @@ navTitle: 'Portfolio'
 
 Here are some of my projects.
 
-- [Fellow Creatures](/portfolio/fellow-creatures)
-- [Upupaepop](/portfolio/upupaepop)
-- [SCA Department](/portfolio/sca-department)
-- [Strelak Community Garden](/portfolio/strelak-community-garden)
+::project-list
+::
