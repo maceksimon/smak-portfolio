fe0aed017a56097a0824fb8163157f1f5abb9c51
Šimon Macek <simon.macek@proficio.cz>
2022-07-17 21:15:24 +0300

    update content and jumbotron

--- components/content/Jumbotron.vue
+++ components/content/Jumbotron.vue
@@ -1,30 +1,33 @@
 <template>
-  <div class="not-prose sm:my-8 sm:text-center lg:text-left">
+  <div class="mb-8 sm:mb-12 sm:text-center lg:text-left">
     <h1
-      class="text-4xl font-extrabold tracking-tight text-gray-900 dark:text-gray-50 sm:text-5xl md:text-6xl"
+      class="not-prose text-2xl font-extrabold tracking-tight text-gray-900 dark:text-gray-50 sm:text-3xl md:text-4xl"
     >
       <Markdown :use="$slots.title" unwrap="p" />
     </h1>
     <div
-      class="mt-3 text-base text-gray-500 dark:text-gray-300 sm:mx-auto sm:mt-5 sm:max-w-xl sm:text-lg md:mt-5 md:text-xl lg:mx-0"
+      class="mt-4 text-base leading-loose text-gray-500 dark:text-gray-300 sm:mx-auto sm:mt-5 sm:max-w-xl sm:text-lg md:mt-5 lg:mx-0"
     >
       <Markdown :use="$slots.perex" unwrap="p" />
     </div>
-    <div class="mt-6 sm:mt-12 sm:flex sm:justify-center lg:justify-start">
+    <div
+      v-if="button1 || button2"
+      class="not-prose mt-6 sm:mt-10 sm:flex sm:justify-center lg:justify-start"
+    >
       <div class="rounded-md shadow">
         <a
-          :href="props.link1"
+          :href="link1"
           class="flex w-full items-center justify-center rounded-md border border-transparent bg-purple-600 px-4 py-2 text-base font-semibold text-white hover:bg-purple-700 md:py-3 md:px-7 md:text-lg"
         >
-          {{ props.button1 }}
+          {{ button1 }}
         </a>
       </div>
       <div class="mt-3 sm:mt-0 sm:ml-3">
         <a
-          :href="props.link2"
+          :href="link2"
           class="flex w-full items-center justify-center rounded-md border border-transparent bg-purple-100 px-4 py-2 text-base font-semibold text-purple-700 hover:bg-white md:py-3 md:px-7 md:text-lg"
         >
-          {{ props.button2 }}
+          {{ button2 }}
         </a>
       </div>
     </div>
@@ -38,4 +41,6 @@ const props = defineProps({
   link1: { type: String, default: "" },
   link2: { type: String, default: "" },
 });
+
+const { button1, button2, link1, link2 } = props;
 </script>
