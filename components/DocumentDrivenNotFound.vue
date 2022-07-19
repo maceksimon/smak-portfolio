471a2514e8226d5cbdaa4b5050206870723147a1
Šimon Macek <simon.macek@proficio.cz>
2022-07-19 12:57:02 +0300

    udpate 404 page

--- components/DocumentDrivenNotFound.vue
+++ components/DocumentDrivenNotFound.vue
@@ -3,7 +3,12 @@
     class="m-auto max-w-2xl bg-white px-4 py-10 dark:bg-gray-800 sm:rounded sm:px-8 sm:shadow"
   >
     <main class="prose prose-gray max-w-none dark:prose-invert">
-      <h1>Hi, this page was not found</h1>
+      <h1 class="sr-only">404: Page not found</h1>
+      <Jumbotron>
+        <template v-slot:title>Sorry, we could not find this page</template>
+      </Jumbotron>
+      <img src="/404.gif" alt="Confused Travolta meme" />
+      <div>Try coming back to <a href="/">homepage</a> instead.</div>
     </main>
   </div>
 </template>
