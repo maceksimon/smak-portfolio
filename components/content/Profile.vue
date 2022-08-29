9ed2bab92a0855e596cd516366e959dd7ad9db97
Šimon Macek <simon.macek@proficio.cz>
2022-08-30 00:10:05 +0200

    update about profile

--- components/content/Profile.vue
+++ components/content/Profile.vue
@@ -1,17 +1,14 @@
 <template>
   <div
-    class="not-prose sm:grid-col my-8 grid gap-4 sm:my-12 sm:grid-cols-2 sm:gap-8"
+    class="not-prose my-8 flex flex-col space-y-4 sm:my-12 sm:flex-row sm:space-y-0 sm:space-x-8"
   >
-    <div v-if="image" class="aspect-w-1 aspect-h-1">
+    <div v-if="image" class="aspect-w-1 aspect-h-1 w-40">
       <img class="rounded-sm object-cover" :src="image" :alt="name" />
     </div>
     <div>
       <div v-if="name || role" class="space-y-1 text-lg font-medium leading-6">
         <h2 class="text-lg font-bold sm:text-xl">{{ name }}</h2>
         <p class="text-blue-700 dark:text-blue-400">{{ role }}</p>
-      </div>
-      <div class="mt-2 text-gray-500 dark:text-gray-400">
-        <Markdown :use="$slots.bio" />
       </div>
       <ul
         v-if="twitterUrl || linkedinUrl"
