8ef09416197befee5ef4e0563f9a57d1c51e0ef7
Šimon Macek <simon.macek@proficio.cz>
2022-08-31 11:01:45 +0200

    tweak error colour

--- components/ErrorDisplay.vue
+++ components/ErrorDisplay.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     v-if="validator.$errors.length"
-    class="absolute -bottom-6 text-sm text-orange-700 dark:text-orange-200"
+    class="absolute -bottom-6 text-sm text-orange-700 dark:text-orange-300"
   >
     <span v-for="error in validator.$errors" :key="error.$validator">
       {{ error.$message }}
