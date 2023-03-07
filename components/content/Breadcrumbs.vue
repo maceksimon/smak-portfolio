a55df75b2871da7e4b083441537c7e4932b4de9c
Šimon Macek <simon.macek@proficio.cz>
2023-03-07 11:21:32 +0100

    breadcrumbs: make parameter optional

--- components/content/Breadcrumbs.vue
+++ components/content/Breadcrumbs.vue
@@ -1,6 +1,6 @@
 <template>
   <nav
-    :class="{ hidden: page.hideBreadcrumb }"
+    :class="{ hidden: page?.hideBreadcrumb }"
     class="not-prose mb-4 flex sm:mb-8"
     aria-label="Breadcrumb"
   >
